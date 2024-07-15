02dd95ef7858565e52b8d310a8c9dc8a22fc4941
Leo Grinsztajn <lgrinszt@margaret.saclay.inria.fr>
2024-07-15 19:26:20 +0200

    benchmark performance enhancing methods

---
+++ evaluate_method_args_classif.py
@@ -0,0 +1,136 @@
+
+import os
+import pickle
+import pickle
+import submitit
+
+# synthcity absolute
+from synthcity.benchmark import Benchmarks
+
+import openml
+# import GenericDataLoader
+from synthcity.plugins.core.dataloader import GenericDataLoader
+import os
+from synthcity_addons import generators
+from sklearn.preprocessing import QuantileTransformer
+from sklearn.preprocessing import LabelEncoder
+import fire
+import pandas as pd
+import numpy as np
+import json
+import hashlib
+from synthcity.plugins import Plugins
+
+
+
+def run_model_on_dataset(model_name, task_id, n_synthetic_points=512, results_base_dir="results", normalization="quantile", save_results=True, **kwargs):
+    from synthcity_addons import generators
+    task = openml.tasks.get_task(task_id)
+    dataset = task.get_dataset()
+    dataset_name = dataset.name
+    print(f"Running {model_name} on {dataset_name}")
+    # kwargs are the hyperparameters
+    hp_dic = kwargs
+    hp_dic.update({"strict": False})
+    print("hp_dic", hp_dic)
+    hp_dic_original = hp_dic.copy()
+    hp_str = "_".join([f"{k}_{v}" for k, v in hp_dic.items()]) # before it's modified by synthcity
+    X, y, _, _ = dataset.get_data(target=dataset.default_target_attribute)
+    # restrict X to 20K random samples
+    if X.shape[0] > 20000:
+        X = X.sample(20000, random_state=42)
+
+    print(X.columns)
+    if normalization == "quantile":
+        X = QuantileTransformer(n_quantiles=100, random_state=42).set_output(transform="pandas").fit_transform(X)
+
+    # add target to X
+    X["target"] = y
+    # label encode target
+    X["target"] = LabelEncoder().fit_transform(X["target"])
+
+    # restrict to 30 columns
+    X = X.iloc[:, -30:]
+    print(X)
+    # take 1024 random rows from X
+    indices = np.random.choice(X.index, 1024, replace=False)
+    X_ref = X.loc[indices]
+    X = X.drop(index=indices)
+
+    loader = GenericDataLoader(X, target_column="target")
+    loader_ref = GenericDataLoader(X_ref, target_column="target")
+
+    print("Loaded")
+
+    task_type = "classification"
+    synthetic_size = n_synthetic_points
+
+    config = {"model_name": model_name, "task_id": task_id, "normalization": normalization, "X_true_n_rows": X.shape[0], 
+              "X_true_n_cols": X.shape[1], "synthetic_size": synthetic_size, "task_type": task_type}
+    config.update(hp_dic_original)
+    # hash config
+    config_hash = hashlib.sha256(pickle.dumps(config)).hexdigest()[:16]
+    if save_results or "tabpfn" in model_name:  
+        os.makedirs(results_base_dir, exist_ok=True)
+        os.makedirs(f"{results_base_dir}/{dataset_name}", exist_ok=True)
+        os.makedirs(f"{results_base_dir}/{dataset_name}/{model_name}", exist_ok=True)
+        os.makedirs(f"{results_base_dir}/{dataset_name}/{model_name}/{config_hash}", exist_ok=True)
+
+    if "tabpfn" in model_name:
+        hp_dic["store_animation_path"] = f"{results_base_dir}/{dataset_name}/{model_name}/{config_hash}/animation.mp4"
+        hp_dic["store_intermediate_data"] = True
+
+
+
+    score = Benchmarks.evaluate(
+        [(model_name, model_name, hp_dic)],
+        loader,
+        X_ref=loader_ref,
+        synthetic_size=synthetic_size,
+        repeats=3,
+        task_type=task_type,
+        metrics = {
+                    'sanity': ['data_mismatch', 'common_rows_proportion', 'close_values_probability', 'distant_values_probability',
+                               'nearest_syn_neighbor_distance', "nearest_real_neighbor_distance",
+                                "nearest_real_neighbor_distance_no_norm", "nearest_syn_neighbor_distance_no_norm",
+                                "nearest_real_neighbor_distance_no_norm_on_train", "nearest_syn_neighbor_distance_no_norm_on_train",
+                                "nearest_real_neighbor_distance_on_train", "nearest_syn_neighbor_distance_on_train"
+                               ],
+                    'stats': ['jensenshannon_dist', 'chi_squared_test', 'feature_corr', 'inv_kl_divergence', 'ks_test', 'max_mean_discrepancy', 'wasserstein_dist', 'prdc', 'alpha_precision', 'survival_km_distance'],
+                    'performance': ['xgb', "mlp"],
+                    'detection': ['detection_xgb'],
+                    'privacy': ['delta-presence', 'k-anonymization', 'k-map', 'distinct l-diversity', 'identifiability_score']
+                }
+    )[model_name]
+    # score_train = Benchmarks.evaluate(
+    #     [(model_name, model_name, hp_dic)],
+    #     loader,
+    #     loader,
+    #     synthetic_size=synthetic_size,
+    #     repeats=3,
+    #     task_type=task_type
+    # )[model_name]
+
+    score = pd.DataFrame(score)
+    #score_train = pd.DataFrame(score_train)
+
+
+    print("Benchmark done")
+    if save_results:
+        # save the results in results/dataset_id/model_name.pkl
+        # create the folders if they don't exist
+        # save the config
+        # with open(f"{results_base_dir}/{dataset_name}/{config_hash}/config.pkl", "wb") as f:
+        #     pickle.dump(config, f)
+        # save config as json
+        with open(f"{results_base_dir}/{dataset_name}/{model_name}/{config_hash}/config.json", "w") as f:
+            json.dump(config, f)
+        # save the scores
+        score.to_csv(f"{results_base_dir}/{dataset_name}/{model_name}/{config_hash}/scores.csv")
+        #score_train.to_csv(f"{results_base_dir}/{dataset_name}/{model_name}/{config_hash}/scores_train.csv")
+    else:
+        return score#, score_train
+
+
+if __name__ == "__main__":
+    print(fire.Fire(run_model_on_dataset))
--- synthcity_addons.py
+++ synthcity_addons.py
@@ -26,6 +26,8 @@ from imblearn.over_sampling import SMOTE
 from utils import create_animation
 from modules import ResNet, MLP
 import ot
+from sklearn.svm import SVC
+from sklearn.cluster import KMeans
 
 class gaussian_noise_plugin(Plugin):
     """Gaussian Noise integration in synthcity."""
@@ -148,6 +150,7 @@ class tabpfn_points_plugin(Plugin):
         store_intermediate_data: bool = False,
         store_animation_path: bool = None,
         n_test_from_false_train: int = 0,
+        n_true_in_train_performance: int = 0,
         n_random_features_to_add: int = 1,
         random_test_points_scale: float = 2,
         init_scale_factor: float = 5,
@@ -155,6 +158,7 @@ class tabpfn_points_plugin(Plugin):
         preprocessing: str = "standard",
         loss: str = "individual",
         task_type: str = "classification",
+        opposite_loss: bool = False,
         n_points: int = 512,
         **kwargs: Any
     ) -> None:
@@ -174,6 +178,7 @@ class tabpfn_points_plugin(Plugin):
         self.store_intermediate_data = store_intermediate_data
         self.store_animation_path = store_animation_path
         self.n_test_from_false_train = n_test_from_false_train
+        self.n_true_in_train_performance = n_true_in_train_performance
         self.n_random_features_to_add = n_random_features_to_add
         self.random_test_points_scale = random_test_points_scale
         if initialization_strategy == "gaussian_noise":
@@ -185,6 +190,7 @@ class tabpfn_points_plugin(Plugin):
         self.init_scale_factor = init_scale_factor
         self.noise_std = noise_std
         self.loss = loss
+        self.opposite_loss = opposite_loss
         self.n_points = n_points
         self.task_type = task_type
         if store_animation_path is not None:
@@ -355,6 +361,11 @@ class tabpfn_points_plugin(Plugin):
                     y_train = torch.cat([torch.ones(self.X_false_train_list[label].shape[0]) * label for label in range(len(self.X_false_train_list))], dim=0)
                     self.X_false_train = torch.cat((X_train, y_train.reshape(-1, 1).to(self.device)), dim=1)
 
+                    if self.n_true_in_train_performance > 0:
+                        indices_true_train = np.random.choice(X_true.shape[0], self.n_true_in_train_performance, replace=False)
+                        X_train = torch.cat((X_train, X_true[indices_true_train]), dim=0)
+                        y_train = torch.cat((y_train, y_true[indices_true_train].cpu()), dim=0)
+
                     perm_train = torch.randperm(X_train.shape[0])
                     X_train = X_train[perm_train]
                     y_train = y_train[perm_train]
@@ -368,6 +379,8 @@ class tabpfn_points_plugin(Plugin):
                     loss_ = torch.nn.functional.cross_entropy(tabpfn_output_log_proba, y_test)
                     loss_list.append(loss_)
                 loss = torch.mean(torch.stack(loss_list))
+                if self.opposite_loss:
+                    loss = -loss
 
                     
 
@@ -419,6 +432,304 @@ class tabpfn_points_plugin(Plugin):
         return self._safe_generate(self.sample, count, syn_schema)
     
 
+class tabpfn_points_performance_plugin(Plugin):
+    """TabPFN integration in synthcity."""
+
+    def __init__(
+        self,
+        device: str = "cuda:0",
+        n_batches: int = 200,
+        lr: float = 0.1,
+        n_permutations: int = 3,
+        n_ensembles: int = 3,
+        initialization_strategy: str = "uniform",
+        store_intermediate_data: bool = False,
+        store_animation_path: bool = None,
+        n_test_from_false_train: int = 0,
+        n_true_in_train_performance: int = 0,
+        init_scale_factor: float = 5,
+        noise_std: float = 0.1,
+        preprocessing: str = "standard",
+        loss: str = "individual",
+        opposite_loss: bool = False,
+        n_points_to_create: int = 512,
+        **kwargs: Any
+    ) -> None:
+        super().__init__(**kwargs)
+        self.device = device
+        self.n_batches = n_batches
+        self.lr = lr
+        self.n_permutations = n_permutations
+        self.n_ensembles = n_ensembles
+        if preprocessing == "standard":
+            self.preprocessor = StandardScaler()
+        elif preprocessing == "none":
+            self.preprocessor = None
+        else:
+            raise ValueError(f"Preprocessing {preprocessing} not supported")
+        self.store_intermediate_data = store_intermediate_data
+        self.store_animation_path = store_animation_path
+        self.n_test_from_false_train = n_test_from_false_train
+        self.n_true_in_train_performance = n_true_in_train_performance
+        if initialization_strategy == "gaussian_noise":
+            self.initialization_strategy =  Plugins().get("gaussian_noise", strict=False)
+        elif initialization_strategy == "smote":
+            self.initialization_strategy =  Plugins().get("smote", strict=False)
+        else:   
+            self.initialization_strategy = initialization_strategy
+        self.init_scale_factor = init_scale_factor
+        self.noise_std = noise_std
+        self.loss = loss
+        self.opposite_loss = opposite_loss
+        self.n_points_to_create = n_points_to_create
+        if store_animation_path is not None:
+            assert store_intermediate_data, "store_intermediate_data must be True to store animation data"
+        if store_intermediate_data:
+            self.loss_list = []
+            self.all_X_false_train = []
+
+
+    @staticmethod
+    def name() -> str:
+        return "tabpfn_points_performance"
+
+    @staticmethod
+    def type() -> str:
+        return "debug"
+
+    @staticmethod
+    def hyperparameter_space(**kwargs: Any) -> List[Distribution]:
+        """
+        We can customize the hyperparameter space, and use it in AutoML benchmarks.
+        """
+        #TODO
+        return [
+            IntegerDistribution(name="embedding_n_units", low=100, high=500, step=50),
+            IntegerDistribution(name="batch_size", low=100, high=300, step=50),
+            IntegerDistribution(name="n_iter", low=100, high=500, step=50),
+        ]
+
+    def _create_init_points(self, n_points_to_create: int, X_true: DataLoader, X_false_train_init: torch.Tensor = None) -> torch.Tensor:
+        if X_false_train_init is None:
+            if self.initialization_strategy == "uniform":
+                X_false_train = (np.random.rand(n_points_to_create, X_true.shape[1]) * 2 * self.random_test_points_scale - self.random_test_points_scale) / self.init_scale_factor
+            elif self.initialization_strategy == "gaussian_noise":
+                #TODO use plugin to generate noise
+                indices = np.random.choice(X_true.shape[0], n_points_to_create, replace=False)
+                X_false_train = X_true[indices]
+                noise = np.random.normal(0, self.noise_std, X_false_train.shape)
+                X_false_train += noise
+            elif type(self.initialization_strategy) != str:
+                # in this case it should be a plugin
+                self.initialization_strategy.fit(pd.DataFrame(X_true))
+                X_false_train = self.initialization_strategy.generate(n_points_to_create).numpy()
+            else:
+                raise ValueError(f"Initialization strategy {self.initialization_strategy} not supported")
+        else:
+            X_false_train = X_false_train_init
+        X_false_train = torch.tensor(X_false_train).float().to(self.device)
+        X_false_train.requires_grad = True
+        return X_false_train
+
+    def _fit(self, X: DataLoader, X_false_train_init=None, *args: Any, **kwargs: Any) -> "tabpfn_points_plugin":
+        X_true, y_true =  X.unpack(as_numpy=True)
+        # save proportions of classes
+        self.class_proportions = np.unique(y_true, return_counts=True)[1] / len(y_true)
+        # make y_true a tensor
+        y_true = torch.tensor(y_true).long().to(self.device)
+
+        if self.preprocessor is not None:
+            X_true = self.preprocessor.fit_transform(X_true) # all numerical features for now
+
+        # in this case we generate one X_false_train for each label
+        self.X_false_train_list = []
+        labels = torch.unique(y_true)
+        #TODO check ordinal encoded labels
+        for label in labels:
+            self.X_false_train_list.append(self._create_init_points(self.n_points_to_create // len(labels), X_true, X_false_train_init))
+        
+
+
+        X_true = torch.tensor(X_true, dtype=torch.float32).to(self.device)
+
+        optimizer = torch.optim.Adam(self.X_false_train_list, lr=self.lr)
+
+        tabpfn_classifier = TabPFNClassifier(device=self.device, N_ensemble_configurations=self.n_permutations,
+                                              no_preprocess_mode=True, no_grad=False, normalize=False)
+        
+        X_train = torch.cat([self.X_false_train_list[label] for label in range(len(self.X_false_train_list))], dim=0)
+        y_train = torch.cat([torch.ones(self.X_false_train_list[label].shape[0]) * label for label in range(len(self.X_false_train_list))], dim=0)
+        self.X_false_train = torch.cat((X_train, y_train.reshape(-1, 1).to(self.device)), dim=1)
+        
+        if self.store_intermediate_data:
+            self.all_X_false_train.append(self.X_false_train.detach().cpu().numpy())
+
+        
+        for batch in tqdm(range(self.n_batches)):
+            n_test = min(2048, X_true.shape[0])
+            loss_list = []
+            for _ in range(self.n_ensembles): #TODO: make a tabpfn prediction function taking ensemble as parameter (with one dataset)
+                #if self.task_type == "classification":
+                indices_test = np.random.choice(X_true.shape[0], n_test, replace=False)
+                X_test = X_true[indices_test]
+                #if self.task_type == "classification":
+                y_test = y_true[indices_test]
+
+                # create train set using labels and self.X_false_train_list
+                X_train = torch.cat([self.X_false_train_list[label] for label in range(len(self.X_false_train_list))], dim=0)
+                y_train = torch.cat([torch.ones(self.X_false_train_list[label].shape[0]) * label for label in range(len(self.X_false_train_list))], dim=0)
+                self.X_false_train = torch.cat((X_train, y_train.reshape(-1, 1).to(self.device)), dim=1)
+
+                if self.n_true_in_train_performance > 0:
+                    indices_true_train = np.random.choice(X_true.shape[0], self.n_true_in_train_performance, replace=False)
+                    X_train = torch.cat((X_train, X_true[indices_true_train]), dim=0)
+                    y_train = torch.cat((y_train, y_true[indices_true_train].cpu()), dim=0)
+
+                perm_train = torch.randperm(X_train.shape[0])
+                X_train = X_train[perm_train]
+                y_train = y_train[perm_train]
+                perm_test = torch.randperm(X_test.shape[0])
+                X_test = X_test[perm_test]
+                y_test = y_test[perm_test]
+
+                tabpfn_classifier.fit(X_train, y_train, overwrite_warning=True)
+
+                tabpfn_output_log_proba = torch.log(tabpfn_classifier.predict_proba(X_test) + 1e-7)
+                loss_ = torch.nn.functional.cross_entropy(tabpfn_output_log_proba, y_test)
+                loss_list.append(loss_)
+            loss = torch.mean(torch.stack(loss_list))
+            if self.opposite_loss:
+                loss = -loss
+
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch % 10 == 0:
+                print(f"Batch {batch} loss: {loss.item()}")
+
+            if self.store_intermediate_data:
+                self.loss_list.append(loss.item())
+                self.all_X_false_train.append(self.X_false_train.detach().cpu().numpy())
+
+        if self.store_animation_path is not None:
+            create_animation(self.all_X_false_train, X_test.detach().cpu().numpy(), self.store_animation_path,
+                                step=max(self.n_batches // 20, 5))
+    
+        return None
+
+    def sample(self, count: int, **kwargs: Any) -> pd.DataFrame:
+        if count > len(self.X_false_train):
+            raise ValueError("Requested count exceeds the available data.")
+        indices = np.random.choice(len(self.X_false_train), count, replace=False)
+        false_points = self.X_false_train[indices].detach().cpu().numpy()
+        if self.preprocessor is not None:
+            return self.preprocessor.inverse_transform(false_points)
+        else:
+            return false_points
+    
+    def _generate(self, count: int, syn_schema: Schema, **kwargs: Any) -> pd.DataFrame:
+        return self._safe_generate(self.sample, count, syn_schema)
+    
+
+class svm_plugin(Plugin):
+    """SVM integration in synthcity."""
+
+    def __init__(self, kernel: str = 'rbf', gamma: str = 'scale', C=1., **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.kernel = kernel
+        self.gamma = gamma
+        self.C = C
+        self.svm = SVC(kernel=self.kernel, gamma=self.gamma, C=self.C)
+    
+    @staticmethod
+    def name() -> str:
+        return "svm"
+    
+    @staticmethod
+    def type() -> str:
+        return "debug"
+
+    @staticmethod
+    def hyperparameter_space(**kwargs: Any) -> List[Distribution]:
+        """
+        We can customize the hyperparameter space, and use it in AutoML benchmarks.
+        """
+        #TODO
+        return [
+            IntegerDistribution(name="embedding_n_units", low=100, high=500, step=50),
+            IntegerDistribution(name="batch_size", low=100, high=300, step=50),
+            IntegerDistribution(name="n_iter", low=100, high=500, step=50),
+        ]
+
+    def _fit(self, X: pd.DataFrame, **kwargs: Any) -> None:
+        X_true, y_true =  X.unpack(as_numpy=True)
+        self.svm.fit(X_true, y_true)
+
+    def sample(self, count: int, **kwargs: Any) -> pd.DataFrame:
+        support_vectors = self.svm.support_vectors_
+        y = self.svm.predict(support_vectors)
+        # concat support_vectors and y
+        X_false_train = np.concatenate((support_vectors, y.reshape(-1, 1)), axis=1)
+        if count > X_false_train.shape[0]:
+            raise ValueError("Requested count exceeds the available data.")
+        indices = np.random.choice(X_false_train.shape[0], count, replace=False)
+        return X_false_train[indices]
+    
+    def get_n_points_to_create(self) -> int:
+        return len(self.svm.support_vectors_)
+    
+    def _generate(self, count: int, syn_schema: Schema, **kwargs: Any) -> pd.DataFrame:
+        return self._safe_generate(self.sample, count, syn_schema)
+
+class kmeans_plugin(Plugin):
+    """KMeans integration in synthcity."""
+
+    def __init__(self, n_points_to_create: int = 8, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.n_points_to_create = n_points_to_create
+        self.kmeans = KMeans(n_clusters=self.n_points_to_create)
+    
+    @staticmethod
+    def name() -> str:
+        return "kmeans"
+    
+    @staticmethod
+    def type() -> str:
+        return "debug"
+
+    @staticmethod
+    def hyperparameter_space(**kwargs: Any) -> List[Distribution]:
+        """
+        We can customize the hyperparameter space, and use it in AutoML benchmarks.
+        """
+        #TODO
+        return [
+            IntegerDistribution(name="n_points_to_create", low=2, high=20, step=1),
+        ]
+
+    def _fit(self, X: pd.DataFrame, **kwargs: Any) -> None:
+        X_true, y_true = X.unpack(as_numpy=True)
+        self.kmeans.fit(X_true)
+        self.cluster_labels = np.zeros(self.n_points_to_create)
+        # for each cluster, assign a label
+        for cluster_label in range(self.n_points_to_create):
+            cluster_indices = np.where(self.kmeans.labels_ == cluster_label)[0]
+            cluster_labels, counts = np.unique(y_true[cluster_indices], return_counts=True)
+            self.cluster_labels[cluster_label] = cluster_labels[np.argmax(counts)]
+        
+    def sample(self, count: int, **kwargs: Any) -> pd.DataFrame:
+        cluster_centers = self.kmeans.cluster_centers_
+        if count > len(cluster_centers):
+            raise ValueError("Requested count exceeds the available data.")
+        indices = np.random.choice(len(cluster_centers), count, replace=False)
+        X_false = cluster_centers[indices]
+        y_false = self.cluster_labels[indices]
+        return np.concatenate((X_false, y_false.reshape(-1, 1)), axis=1)
+    
+    def _generate(self, count: int, syn_schema: Schema, **kwargs: Any) -> pd.DataFrame:
+        return self._safe_generate(self.sample, count, syn_schema)
 
 
 class tabpfn_generator_plugin(Plugin):
@@ -810,10 +1121,12 @@ class smote_imblearn_plugin(Plugin):
 generators = Plugins()
 
 generators.add("tabpfn_points", tabpfn_points_plugin)
+generators.add("tabpfn_points_performance", tabpfn_points_performance_plugin)
 generators.add("tabpfn_generator", tabpfn_generator_plugin)
 generators.add("forest_diffusion", forest_diffusion_plugin)
 generators.add("smote", smote_plugin)
 generators.add("smote_imblearn", smote_imblearn_plugin)
 generators.add("gaussian_noise", gaussian_noise_plugin)
 generators.add("oracle", oracle_plugin)
-
+generators.add("svm", svm_plugin)
+generators.add("kmeans", kmeans_plugin)
